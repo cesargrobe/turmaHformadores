164ca4980a6a98dfbaa4859e8d9d80e071aaddd2
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2023-10-19 19:35:20 -0300

    Aula 19/10 b

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function tocaSomRisada() {
     document.querySelector("#som_tecla_aplausos ").play();
 }
 
-const listaDeTeclas = document.querySelectorAll(".teclado");
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 listaDeTeclas[2].onclick = tocaSomRisada;
 
